9c3bcc370a0154420463f21912c47d8b8a33bd4c
eknaian <eknaian@gmail.com>
2022-06-16 15:27:39 -0400

    init commit

---
+++ src/store/index.js
@@ -0,0 +1,48 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
+const initialCounterState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialCounterState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+const initialAuthState = {
+  isAuthed: false,
+  reducers: {
+    login(state) {
+      state.isAuthed = true;
+    },
+    logout(state) {
+      state.isAuthed = false;
+    },
+  },
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialAuthState,
+});
+
+const store = configureStore({
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+});
+
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
+
+export default store;
